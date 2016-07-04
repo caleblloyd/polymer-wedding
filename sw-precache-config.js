217325f78d3b2ae9ec8aa8a21475d39d38fa7da8
Caleb Lloyd <caleblloyd@gmail.com>
2016-07-04 17:01:30 -0400

    story

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,9 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/images/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}',
+    '/src/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'
   ],
   navigateFallback: '/index.html'
 };
