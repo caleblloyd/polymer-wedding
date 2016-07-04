7648f5cf86b3193d9f1295343feff0f5fdb7195d
Caleb Lloyd <caleblloyd@gmail.com>
2016-07-04 17:44:09 -0400

    commit fonts for sw

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,8 +3,8 @@ module.exports = {
     '/index.html',
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
-    '/images/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}',
-    '/src/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'
+    '/images/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff,woff2}',
+    '/src/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff,woff2}'
   ],
   navigateFallback: '/index.html'
 };
